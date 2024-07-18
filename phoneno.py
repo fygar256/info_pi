02549d32ba3631a1659d76b04a2f48c199892fa7
FYGAR256 <fygar256@gmail.com>
2024-07-18 13:02:59 +0900

    Add files via upload

---
+++ phoneno.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import os
+import binascii
+
+def main():
+    f=open("/dev/random",'rb') # /dev/randomを開く
+    while(1):
+        randomdata=f.read(1) # 1バイトの真性乱数を読み出し
+
+        randomhex=binascii.hexlify(randomdata) #１６進の文字列に変換
+        randomint=int(randomhex,16) # 整数に変換
+        i='0123456789 '
+        n=int(randomint/256*len(i))
+        print(i[n],end='')
+    f.close() # /dev/randomを閉じる
+    return
+
+if __name__=='__main__':
+	main()
+	exit(0)
+
+
